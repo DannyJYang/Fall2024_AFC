8747cbd37f9725572a28115490ef214a5b4662f5
JinHong Yang <jinhong.yang.mil@army.mil>
2024-08-27 13:02:19 -0500

    Stopped at question 20

--- JavaScriptLab1/script.js
+++ JavaScriptLab1/script.js
@@ -14,5 +14,37 @@ let fullName = `${firstName} ${lastName}`
 let colors = ['red' , 'green' , 'blue']
 // 7. Access the first element of the `colors` array and assign it to a variable `firstColor`
 let firstColor = colors[0];
-
-console.log[12];
+// 8. Change the second element of the `colors` array to 'yellow'
+colors[1] = 'yellow'
+// 9. Add 'purple' to the end of the `colors` array
+colors.push("purple");
+// 10. Find the length of the `colors` array and assign it to a variable `colorsLength`
+let colorsLength = colors.length;
+// 11. Create an object named `person` with properties `name` (string), `age` (number), and `isStudent` (boolean)
+let person = {
+    name: "John Smith", 
+    age: 25 ,
+    isStudent: true
+};
+// 12. Access the `name` property of the `person` object and assign it to a variable `personName`
+let personName = person.name;
+// 13. Change the `age` property of the `person` object to 35
+person.age = 35;
+// 14. Add a new property `hobby` with the value 'reading' to the `person` object
+person.hobby = "reading"
+// 15. Delete the `isStudent` property from the `person` object
+delete person.isStudent
+// 16. Write an if statement that checks if `age` is greater than 18, and if so, log "Adult" to the console
+// if (person.age > 18)
+//     console.log("Adult")
+// 17. Write an if-else statement that checks if `isStudent` is true, and if so, log "Student" to the console, otherwise log "Not a student"
+console.log(person.isStudent == true ? 'Student' : 'Not a Student')
+// 18. Write an if-else if-else statement that checks if `age` is less than 13, log "Child", if age is between 13 and 19 log "Teenager", otherwise log "Adult"
+let checkWhatAgeGroup = person.age < 13 ? 'Child':
+                        (person.age >= 13 && person.age <= 19) ? 'Teenager': 
+                        'Adult';
+console.log(checkWhatAgeGroup)
+// 19. Write a condition using the logical AND operator to check if `age` is greater than 18 and `isStudent` is true, and if so, log "Adult Student"
+if(person.age > 18 && person.isStudent == true)
+    console.log("Adult Student")
+// console.log(result)
