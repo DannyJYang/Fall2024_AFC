de068921fa75e2e100a65ede55201793dc5b6fe4
JinHong Yang <jinhong.yang.mil@army.mil>
2024-08-27 11:59:58 -0500

    Changed the name of the repository to JavaScriptLab1. Still working on finishing the lab.

---
+++ JavaScriptLab1/script.js
@@ -0,0 +1,18 @@
+// 1. Declare a variable named `age` and assign it the value 25
+let age = 25;
+// 2. Declare a variable named `name` and assign it your name
+let name = 'Danny'
+// 3. Declare a variable named `isStudent` and assign it the value true
+let isStudent = true
+// 4. Reassign the value of `age` to 30
+age = 30
+// 5. Declare two variables `firstName` and `lastName`, and assign them values. Then, create a third variable `fullName` that concatenates both.
+let firstName = "Danny"
+let lastName = "Yang"
+let fullName = `${firstName} ${lastName}`
+// 6. Create an array named `colors` with the values 'red', 'green', and 'blue'
+let colors = ['red' , 'green' , 'blue']
+// 7. Access the first element of the `colors` array and assign it to a variable `firstColor`
+let firstColor = colors[0];
+
+console.log[12];
