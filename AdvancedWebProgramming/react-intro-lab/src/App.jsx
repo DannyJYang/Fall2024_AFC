10da79ae47b829a802d8868e1b2ba8f7ba9acf94
JinHong Yang <jinhong.yang.mil@army.mil>
2024-09-25 09:20:59 -0500

    updating react intro lab

--- AdvancedWebProgramming/react-intro-lab/src/App.jsx
+++ AdvancedWebProgramming/react-intro-lab/src/App.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Greeting from "./components/Greeting";
 import UserCard from "./components/UserCard";
+import CardContainer from "./components/CardContainer"
 
 const App = () => {
-
+  let info = [
+    {name: "Bob", age: "30"},
+    {name: "Charlie", age: "35"},
+    {name: "Alice", age:"25"}
+  ];
+  let userCards = info.map((user, i) => {
+    return <UserCrard key={index} name={user.name} age={user.age}/>;
+  });
 
 
   return (
     <div>
       <Greeting name="Danny" />
-      <UserCard name="Bob" age="30"/>
-      <UserCard name="Charlie" age="35"/>
-      <UserCard name="Alice" age="25"/>
+      <CardContainer children={userCards}/>
     </div>
   );
 
---
+++ AdvancedWebProgramming/react-intro-lab/src/components/CardContainer.jsx
@@ -0,0 +1,10 @@
+const CardContainer = (children) => {
+    let {CardContainer} = children;
+    return (
+        <div style= {{border: "2px solid", padding: "20px"}}>
+            {CardContainer}
+        </div>
+    )
+}
+
+export default CardContainer;
