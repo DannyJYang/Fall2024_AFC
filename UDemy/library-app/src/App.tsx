af62beaf985ce47062b45a4b866d5b0f533a7e50
JinHong Yang <jinhong.yang.mil@army.mil>
2024-10-21 09:23:00 -0500

    Adding Intermediate Web Programming directory and bucket list project

---
+++ UDemy/library-app/src/App.tsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+import './App.css'
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar.tsx";
+
+function App() {
+
+
+    return (
+        <Navbar/>
+    )
+}
+
+export default App
